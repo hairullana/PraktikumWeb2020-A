3def7677121c88662d7e54c2adf84ded48b6b59e
Hairul Lana <hairullana99@gmail.com>
2020-12-18 19:45:56 +0800

    upload uas

---
+++ UAS-KELOMPOK1/assets/js/script.js
@@ -0,0 +1,20 @@
+// scroll
+$(window).scroll(function () {
+
+    var wScroll = $(this).scrollTop();
+
+    // jumbotron
+    $('.jumbotron-index h1').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+    });
+
+    $('.jumbotron-index p').css({
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+    });
+
+    $('.jumbotron-index button').css({
+        'transform': 'translate(0px, ' + wScroll / 1.5 + '%)'
+    });
+
+
+});
---
+++ UAS-KELOMPOK1/registrasi.php
@@ -0,0 +1,69 @@
+<?php
+
+// panggil koneksi db
+require "db.php";
+// panggil file functions.php
+require "functions.php";
+// aktifkan session
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi</title>
+    <?php require "headtags.php" ?>
+</head>
+<body>
+    
+    <!-- navbar -->
+    <?php require "navbar.php" ?>
+
+
+    <!-- pilihan registrasi -->
+    <div class="row mt-5">
+        <div class="col-md-6 offset-md-3">
+            <div class="container">
+                <div class="card shadow-lg">
+                    <div class="card-header text-center">
+                        <h3>Registrasi</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="container">
+                            <div class="row text-center">
+                                <div class="col">
+                                    <a href="registrasi-perusahaan.php"><button class="btn btn-primary btn-lg btn-block">Registrasi Sebagai Perusahaan</button></a>
+                                </div>
+                                <div class="col">
+                                    <a href="registrasi-pelamar.php"><button class="btn btn-primary btn-lg btn-block">Registrasi Sebagai Pelamar</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- footer -->
+    <?php
+    include 'footer.php';
+    ?>
+    <!-- end footer -->
+
+</body>
+</html>
+
+
+<?php
+
+// cek apakah sudah login atau belum
+cekSudahLogin();
+
+?>
